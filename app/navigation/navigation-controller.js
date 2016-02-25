a85b4a77e5ba53d38c739a72a64e312dd7ca65d9
Alejandra León <alejandramleon@unitec.edu>
2016-02-25 02:06:17 -0600

    segundo commit

--- app/navigation/navigation-controller.js
+++ app/navigation/navigation-controller.js
@@ -2,8 +2,16 @@
 	angular.module('WebDev')
 	.controller('NavigationController', ['$scope','$http','$state', function($scope, $http, $state){
 		$scope.logUserIn = function(){
+			
+			if(localStorage['User-Data']){
+				$scope.loggedIn = true;
+			} else{
+				$scope.loggedIn = false;
+			}
+
 			$http.post('api/user/login', $scope.login).success(function(response){
 				localStorage.setItem('User-Data', JSON.stringify(response));
+				$scope.loggedIn = true;
 			}).error(function(error){
 				console.error(error);
 			});
--- server/controllers/authentication-controller.js
+++ server/controllers/authentication-controller.js
@@ -14,7 +14,10 @@ module.exports.login = function(req, res){
 			console.log("Error out");
 		}
 		if(results && results.length == 1){
-			res.json(req.body.email);
+			var userData = results[0];
+			res.json({email: req.body.email, 
+					  _id: userData._id});
+			//console.log(results);
 		}
 	})
 }
